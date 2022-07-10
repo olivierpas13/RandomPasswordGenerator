03577aa1a08a31de5ef4a799ee9a464e68cd4701
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-07-10 15:50:15 -0500

    Added an option of password length and copy to clipboard button

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
+// Char at index
 let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 let symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
-
+ 
 let charactersAllowed = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-let passwordLength = 15
 
+
+let passwordLength = getLength()
 let passwordElOne = document.getElementById("password-el-one")
 let passwordElTwo = document.getElementById("password-el-two")
 let symbolsAllowed = false
 let numbersAllowed = false
 
 
+function getLength(){
+    let select = document.getElementById('password-length');
+    let value = select.options[select.selectedIndex].value;
+    return value
+}
+
+
 function toggleSymbols(){
     // symbolsAllowed = symbolsAllowed ? false : true;
     symbolsAllowed = !symbolsAllowed
-    console.log('Toggled bool is',
-                            symbolsAllowed);
+
 }
 
 function toggleNumbers(){
     numbersAllowed = numbersAllowed ? false : true;
-    console.log('Toggled bool is',
-                            numbersAllowed);
+
 }
 
 
@@ -61,15 +68,14 @@ function assignPassword(){
 }
 
 function generateRandomPassword(){
-    
+    let passwordLength = getLength()
     addNumbers()
     addSymbols()
         let generatedPassword = ""
-        for(let i = 0; i < 15; i++){
-            if(generatedPassword.length < 15){
+        for(let i = 0; i < passwordLength; i++){
+            if(generatedPassword.length < passwordLength){
                 generatedPassword += charactersAllowed[randomIndex()]
-            }else if(generatedPassword.length === 15){
-                generatedPassword = ""
+            }else if(generatedPassword.length === passwordLength){
                 generateRandomPassword()
             }
         }
@@ -80,4 +86,9 @@ function generateRandomPassword(){
 function randomIndex(){
     let rndmIndex = Math.floor(Math.random()*charactersAllowed.length)
     return rndmIndex
+}
+
+function copyOnClick(element){
+    let copyText = element.textContent
+    navigator.clipboard.writeText(copyText)
 }
